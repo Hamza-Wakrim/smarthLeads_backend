f8dd5df7f18f1cd1133f2a11bc25cc27e70319b8
hamzajb <hamza.jebrane94@gmail.com>
2022-05-18 04:15:46 +0100

    Add and configure authentication with passport package, add register/login, add tickets table

---
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserAuthController extends Controller
+{
+    public function register(Request $request): \Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    {
+        $data = $request->validate([
+            'first_name' => 'max:255',
+            'last_name' => 'max:255',
+            'username' => 'required|max:255',
+            'email' => 'email|required',
+            'password' => 'required|confirmed',
+        ]);
+
+        $data['password'] = bcrypt($request->password);
+
+        $user = User::create($data);
+
+        return response(['user' => $user]);
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required',
+        ]);
+
+        if (!auth()->attempt($data)) {
+            return response(['error_message' => 'Incorrect Details. Try again']);
+        }
+
+        $user = auth()->user();
+
+        $token = $user->createToken('API Token')->accessToken;
+
+        return response(['user' => $user, 'token' => $token]);
+    }
+}
